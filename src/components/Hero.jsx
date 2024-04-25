16db949b7ca880de38a727141f40dd5e4a753f3f
knllydgmntz <knllydgmntz@gmail.com>
2024-04-25 21:56:03 +0800

    Adjust text sizes in menu bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
           id="menu"
           className={`${
             toggle ? "flex" : "hidden"
-          } absolute top-0 bottom-0 left-0 flex-col items-center w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black/50 backdrop-blur-sm`}
+          } absolute top-0 bottom-0 left-0 flex-col items-center w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-[35px] text-white uppercase bg-black/50 backdrop-blur-sm`}
         >
           <a href="#" className="hover:text-pink-500">
             About
