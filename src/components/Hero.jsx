1bacc339a19c0698d987c9d2232b4f807f54318a
knllydgmntz <knllydgmntz@gmail.com>
2024-04-25 14:29:16 +0800

    adding function in hamburger menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,14 @@ const Hero = () => {
   const [toggle, setToggle] = useState("");
 
   function clickHandler() {
-    console.log(toggle);
-    setToggle((isToggle) => !isToggle);
+    if (!toggle) {
+      setToggle("open");
+    } else {
+      setToggle(undefined);
+    }
+    console.log("open");
+    // setToggle((isToggle) => !isToggle);
+    // event.preventDefault();
   }
 
   return (
@@ -46,7 +52,7 @@ const Hero = () => {
               onClick={clickHandler}
               id="menu-btn"
               type="button"
-              className="z-40 block hamburger md:hidden focus:outline-none"
+              className={`${toggle} ? "open" : "z-40 block hamburger md:hidden focus:outline-none"}`}
             >
               <span className="hamburger-top"></span>
               <span className="hamburger-middle"></span>
@@ -58,7 +64,7 @@ const Hero = () => {
         {/* Mobile Menu */}
         <div
           id="menu"
-          className="abosulute top-0 bottom-0 left-0 hidden flex-col self-end  w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black"
+          className="hidden abosulute top-0 bottom-0 left-0 flex-col self-end  w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black"
         >
           <a href="#" className="hover:text-pink-500">
             About
